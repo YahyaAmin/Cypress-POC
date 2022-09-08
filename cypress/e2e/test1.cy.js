c45a41b9dc6a47efa7ae88518e398f13bb59a2db
YahyaAmin <yahyaamin1997@gmail.com>
2022-09-08 12:15:10 +0500

    changes

---
+++ cypress/e2e/test1.cy.js
@@ -0,0 +1,8 @@
+
+it('Google Search', function(){
+
+    cy.visit('https://google.com')
+    cy.get('.gLFyf').type("abcdefgh{Enter}")  //For pressing a key
+
+    //cy.contains("Google Search").click()  FOR CLICKING SEARCH
+})
