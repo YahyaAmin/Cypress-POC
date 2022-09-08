62915e47c6c4d4aa858bcc646bb60a72f635e7dd
YahyaAmin <yahyaamin1997@gmail.com>
2022-09-08 19:17:55 +0500

    changes

--- cypress/e2e/pomdemo.cy.js
+++ cypress/e2e/pomdemo.cy.js
@@ -1,11 +1,28 @@
-import {LoginPage} from './Pages/login_page'
+import { LoginPage } from "./Pages/login_page";
 
-const loginPage = new LoginPage()
+const loginPage = new LoginPage();
 
-it('pomdemo', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com')
-    loginPage.enterUsername("Admin")
-    loginPage.enterPassword("admin123")
-    loginPage.clickLogin()
+describe("All Login Tests", () => {
+  //keywords for grouped tests       it.only, it.skip  BeforeEach(can be placed insider or outside describe block)
+  //for running "before each" for all spec files, put it in e2e.js file 
 
-})
+  beforeEach(function () {
+    cy.log("Running Login Tests")
+    cy.visit("https://opensource-demo.orangehrmlive.com");
+  });
+
+  it("Login Test 1", () => {
+    loginPage.enterUsername("Admin");
+    loginPage.enterPassword("admin123");
+    loginPage.clickLogin();
+    cy.get(".oxd-userdropdown-name").click();
+  });
+
+  it("Login Test 2", () => {
+    loginPage.enterUsername("WrongAdmin");
+    loginPage.enterPassword("admin123");
+    loginPage.clickLogin();
+    cy.get(".oxd-userdropdown-name").click();
+  });
+});
+
