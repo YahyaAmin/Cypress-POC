1d825b0f70585dfffc0f8c6d8dcd59ebe8038612
YahyaAmin <yahyaamin1997@gmail.com>
2022-09-12 12:16:04 +0500

    Read/Write

--- cypress/e2e/readwriteFilesDemo.cy.js
+++ cypress/e2e/readwriteFilesDemo.cy.js
@@ -1,3 +1,5 @@
+/// <reference types = "cypress"/>
+
 before(function(){
     cy.fixture('example.json').as('test_data') //to use data as a variable 
 })
@@ -17,5 +19,16 @@ it('Read files using fixtures', function()
 it('read file using ReadFile()', function() {
     cy.readFile('cypress/fixtures/example.json').then((data) => {
     cy.log(data.name)
+    //for json files can use its => .its('name').should('eq','data)
 })
+})
+
+
+it('Write file demo', function() {
+    cy.writeFile('sample.txt', 'I am Yahya')  
+    //if file doesn't exist, then it will be created on root directory
+
+    cy.writeFile('sample.txt', '\nAvoid overwriting example', {flag:'a+'})  
+    //add a flag to avoid overwriting existing content in sample.txt
+    
 })
