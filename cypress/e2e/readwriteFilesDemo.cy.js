cb50e3ca46e36df141bee31e3043a2f1c58e58ee
YahyaAmin <yahyaamin1997@gmail.com>
2022-09-12 11:31:11 +0500

    ReadFile

--- cypress/e2e/readwriteFilesDemo.cy.js
+++ cypress/e2e/readwriteFilesDemo.cy.js
@@ -1,8 +1,21 @@
+before(function(){
+    cy.fixture('example.json').as('test_data') //to use data as a variable 
+})
+
 
 it('Read files using fixtures', function()
 {
-    cy.fixtures('example.json').then((data) => {
+    cy.fixture('example.json').then((data) => {
             cy.log(data.name)
             cy.log(data.email)
     })
+
+    cy.log(this.test_data.name) //to use data as a variable
+
+})
+
+it('read file using ReadFile()', function() {
+    cy.readFile('cypress/fixtures/example.json').then((data) => {
+    cy.log(data.name)
+})
 })
